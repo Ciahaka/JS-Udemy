5771674199886e52e6e4f3e372d0873e17bb6d83
Ciahaka <wilddiver.by@gmail.com>
2022-08-01 20:52:03 +0300

    add_Variables

---
+++ src/Variables and types/Variables and types.js
@@ -0,0 +1,21 @@
+// let myName
+// console.log(myName)
+// myName = 'Sanya'
+// console.log(myName)
+// console.log(console.log(10))
+
+const objectA={
+    a:10,
+    b:20
+}
+const copyOfA = objectA
+copyOfA.a=30
+copyOfA.c= 'abc'
+console.log(copyOfA)
+objectA.c='UFO'
+objectA.b=15
+console.log(copyOfA)
+objectA.d=true
+console.log(objectA)
+
+
