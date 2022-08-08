f83036586b43a3cea3132b2161cbb40c1354aa0b
Ciahaka <wilddiver.by@gmail.com>
2022-08-08 22:01:26 +0300

    add_Concatenation

---
+++ src/Concatenation_String/Concatenation.js
@@ -0,0 +1,8 @@
+const  myName = 'My name Alex'
+const myCity = 'I live in Polotsk City'
+const greetings_1 = myName + myCity
+console.log(greetings_1)
+const greetings_2 = myName + ' ' + myCity
+console.log(greetings_2)
+const greetings = `${myName}. ${myCity}!`
+console.log(greetings)
