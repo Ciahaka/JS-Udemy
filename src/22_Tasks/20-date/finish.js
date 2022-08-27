9f4eea7309522a3eaae1779ec9084ff990ee3f65
Ciahaka <wilddiver.by@gmail.com>
2022-08-27 15:16:49 +0300

    add_tasks_20

---
+++ src/22_Tasks/20-date/finish.js
@@ -0,0 +1,24 @@
+/** ЗАДАЧА 20 - Дата
+ *
+ * 1. Создайте переменную и присвойте ей текущее время в миллисекундах
+ *
+ * 2. Выведите значение в консоль
+ */
+const myDate = new Date
+console.log(myDate)
+const dateMilSec = myDate.getTime()
+console.log(dateMilSec)
+const dateDate = myDate.getDate()
+console.log(dateDate)
+const dateDay = myDate.getDay()
+console.log(dateDay)
+const dateYear = myDate.getFullYear()
+console.log(dateYear)
+
+const newDate = new Date().getMilliseconds()
+console.log(newDate)
+const newDate_2 = new Date().getTime()
+console.log(newDate_2)
+
+
+
