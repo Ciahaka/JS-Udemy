714c2b6712f3103f26991d3ee0ca08631a27d324
Ciahaka <wilddiver.by@gmail.com>
2022-08-28 13:30:51 +0300

    add_BinaryOperators_tasks_24

---
+++ src/22_Tasks/24-alternative-binary-operators/finish.js
@@ -0,0 +1,29 @@
+/** ЗАДАЧА 24 - Альтернативные бинарные операторы
+ *
+ * В каждом выражении замените два оператора на один бинарный
+ */
+
+let a = 10
+let b = 15
+
+a = a + 1
+console.log(a) // 11
+b += 1
+console.log(b)
+console.log('-------')
+a = a * 2
+console.log(a) // 22
+b *=2
+console.log(b)
+console.log('-------')
+
+a = a - 5
+console.log(a) // 17
+b -= 5
+console.log(b)
+console.log('-------')
+
+a = a / 2
+console.log(a) // 8.5
+b /= 2
+console.log(b)
